4fe07be37e21e45f0e191c9cd2a0da47b650b4b4
Seypopi <pierre.alex.bourdais@gmail.com>
2020-07-27 17:51:58 +0200

    random avec argument pour valeur max et retour erreur pour 0 et 1

--- commands/rand.js
+++ commands/rand.js
@@ -4,6 +4,15 @@ module.exports = {
     name: 'rand',
     description: 'rand!',
     execute(msg, args) {
-      msg.reply(randomInt(1, 10));
+        console.log(typeof args[0])
+        if ( args[0] === undefined) {
+            msg.reply(randomInt(1, 10));
+        }
+        else if(args[0] >= 2) {
+            msg.reply(randomInt(1, parseInt(args[0])));
+        } 
+        else if(args[0] >= 0){
+            msg.reply('Donner une valeur supérieure à 1');
+        } 
     },
   };
